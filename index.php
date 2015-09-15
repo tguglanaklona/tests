30e51cfe7fdf6552d347cbad79ecb47f2fb6d51d
tguglanaklona <tguglanaklona@gmail.com>
2015-09-15 21:59:57 +0300

    m

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <html>
 <header></header>
 	<body>
+
 <iframe/sandbox/src="http://www.geographycollector.com"></iframe>
+<script>var/**/iframe=document.createElement('iframe');iframe.src='http://www.geographycollector.com';iframe.style.display='none';document.body.appendChild(iframe);
+</script>
+
 	<details ontoggle=window.open('http://ya.ru');></details>
+
 <img/src="x"/onerror=""> 
+
 	<body onwheel="alert(0)">
+	<script>document.body.setAttribute("","");
+	</script>
+
 <?php
 	phpinfo();
 ?>
 </body>
 </html>
+
 
